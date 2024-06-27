45a8bd4b40f4854c87d35bf8bbfe23ab78fe7ffc
galeysh <galeysh@gmail.com>
2024-06-27 23:02:43 +0200

    add branche HW

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 
 //Homeworks
-
-
+import HW2 from "./HW/HW2/HW2";
 //Consultations
 // import Consultation01 from "./consultations/Consultation01/Consultation01";
 
 function App() {
   return (
     <div className="App">
+     <HW2 />
       {/* <Lesson02 /> */}
       {/* <Consultation01 /> */}
     </div>
---
+++ src/HW/HW2/HW2.js
@@ -0,0 +1,13 @@
+import Card from "../../components/Card/Card"
+function HW2() {
+    return (
+      <div>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
+  }
+  
+  export default HW2;
+  
