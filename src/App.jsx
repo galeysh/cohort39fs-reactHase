c93f26ca797d32b97ca4199d7037457fce9a714b
galeysh <galeysh@gmail.com>
2024-06-30 22:47:06 +0200

    add Lesson03

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
+
 //Lessons
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
+import Lesson03 from "./lessons/Lesson03/Lesson03"
 
 //Homeworks
-import HW2 from "./HW/HW2/HW2";
+// import Homework02 from "./homeworks/Homework02/Homework02";
+
 //Consultations
 // import Consultation01 from "./consultations/Consultation01/Consultation01";
 
 function App() {
   return (
     <div className="App">
-     <HW2 />
       {/* <Lesson02 /> */}
+      {/* <Homework02 /> */}
       {/* <Consultation01 /> */}
+      <Lesson03 />
     </div>
   );
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,38 +1,15 @@
 import "./styles.css";
 
-function Button() {
-  const buttonName = "Send";
-
-  // 2 способ задания класса - через функцию
-  // const getButtonClass = () => {
-  //   return buttonName === "Delete"
-  //     ? "button-component delete-button"
-  //     : "button-component main-button";
-  // };
-
-  //3 способ - замена тернарного опретатора, оператором if
-  // const getButttonClass = () => {
-  //   if (buttonName === "Delete") {
-  //     return "button-component delete-button";
-  //   } else {
-  //     return "button-component main-button";
-  //   }
-  // };
-
-  // 4 способ
-  const buttonClass =
-    buttonName === "Delete"
-      ? "delete-button"
-      : "main-button";
-
-  // 2 способ задания класса - через функцию
-  // return <button className={getButtonClass()}>{buttonName}</button>;
-
-  // 3 способ задания класса - через функцию (if)
-  // return <button className={getButttonClass()}>{buttonName}</button>;
-
-  // 4 способ задания класса -
-  return <button className={`button-component ${buttonClass}`}>{buttonName}</button>;
+function Button({ children, type, buttonName = "Send" }) {
+  const buttonClass = type === "Delete" ? "delete-button" : "main-button";
+  
+  return (
+    <button className={`button-component ${buttonClass}`}>
+      {/* Children example */}
+      {/* {children} */}
+      {buttonName}
+    </button>
+  );
 }
 
 export default Button;
